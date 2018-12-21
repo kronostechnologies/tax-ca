27d1a484f25bdd7d303a26d94dec8738c40e7df2
Kevin Doucet <kdoucet@kronostechnologies.com>
2018-12-21 08:06:05 -0500

    Update rrsp.js

--- src/investments/rrsp.js
+++ src/investments/rrsp.js
@@ -3,7 +3,7 @@ Sources:
 	https://www.canada.ca/en/revenue-agency/services/tax/registered-plans-administrators/pspa/mp-rrsp-dpsp-tfsa-limits-ympe.html
 	http://www.rrq.gouv.qc.ca/SiteCollectionDocuments/www.rrq.gouv.qc/Francais/pdf/rcr/annexe06.pdf
 
-Updated for January 1, 2019
+Revised 2018-12-21
 */
 
 export default { MAX_CONTRIBUTION: 26500 };
