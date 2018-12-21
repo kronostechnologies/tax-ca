dfac2f7b2c71aad9cbfed6f8a25386d3dac4ac8e
Kevin Doucet <kdoucet@kronostechnologies.com>
2018-12-21 07:52:25 -0500

    Update tfsa.js

--- src/investments/tfsa.js
+++ src/investments/tfsa.js
@@ -1,4 +1,11 @@
+/*
+Sources:
+	https://www.canada.ca/en/revenue-agency/services/tax/registered-plans-administrators/pspa/mp-rrsp-dpsp-tfsa-limits-ympe.html
+	
+Updated for January 1, 2019
+*/
+
 export default {
-	MAX_CONTRIBUTION: 5500,
+	MAX_CONTRIBUTION: 6000,
 	ROUNDING_FACTOR: 500
 };
