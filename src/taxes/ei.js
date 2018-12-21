a6b04401d1cf8305e61fe39a2e408712b883a6d3
Kevin Doucet <kdoucet@kronostechnologies.com>
2018-12-21 09:05:18 -0500

    Update ei.js

--- src/taxes/ei.js
+++ src/taxes/ei.js
@@ -1,13 +1,16 @@
 /*
 Sources:
-	https://www.canada.ca/en/revenue-agency/services/tax/businesses/topics/payroll/payroll-deductions-contributions/employment-insurance-ei/ei-premium-rates-maximums.html
+	https://www.canada.ca/en/employment-social-development/programs/ei/ei-list/ei-employers/2019-maximum-insurable-earnings.html
+	https://www.canada.ca/en/employment-social-development/programs/ei/ei-list/reports/premium/rates2019.html
+
+Revised 2018-12-21
 */
 
 export default {
-	MAX_INSURABLE_EARNINGS: 51700,
+	MAX_INSURABLE_EARNINGS: 53100,
 	PREMIUM_RATES: {
-		CA: 0.0166,
-		QC: 0.0130,
+		CA: 0.0162,
+		QC: 0.0125,
 	},
 };
 
