8b289d3d395e528e0f2c6073cc8fa6d2b9983ebe
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2019-10-25 15:35:48 -0400

    Add Jest and utils tests

---
+++ test/utils/collections.test.ts
@@ -0,0 +1,24 @@
+import { maxBy } from '../../src/utils/collections';
+
+describe('maxBy', () => {
+    it('should return maximum value by mapping function', () => {
+        const maxB = { a: 1, b: 5 };
+        const maxA = { a: 5, b: 2 };
+        const values = [maxA, maxB];
+
+        const valueWithMaxB = maxBy(values, x => x.b);
+
+        expect(valueWithMaxB).toBe(maxB);
+    });
+
+    it('should return first value if multiple maximums', () => {
+        const maxA = { a: 5, b: 2 };
+        const maxB = { a: 1, b: 5 };
+        const maxA2 = { ...maxA };
+        const values = [maxA, maxB, maxA2];
+
+        const valueWithMaxA = maxBy(values, x => x.a);
+
+        expect(valueWithMaxA).toBe(maxA);
+    });
+});
---
+++ test/utils/math.test.ts
@@ -0,0 +1,20 @@
+import { roundToPrecision } from '../../src/utils/math';
+
+describe('roundToPrecision', () => {
+    it('should round to integer by default', () => {
+        const roundedValue = roundToPrecision(1.5678);
+
+        expect(roundedValue).toBe(2);
+    });
+
+    it.each`
+        value       | precision | expected
+        ${1.56789}   | ${1}      | ${1.6}
+        ${1.56789}   | ${2}      | ${1.57}
+        ${1.56789}   | ${3}      | ${1.568}
+    `('should round $value with a precision of $precision digits', ({ value, precision, expected }) => {
+        const roundedValue = roundToPrecision(value, precision);
+
+        expect(roundedValue).toBe(expected);
+    });
+});
