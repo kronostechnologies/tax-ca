3a0ebfecf5d08dd8e331286b5f81f17546316030
Toby Daigle <toby.daigle@hotmail.ca>
2020-04-03 07:49:21 -0400

    Use plain javascript to compute monthsDelta, allowing us to remove the heavy moment lib

--- src/utils/tests/date.spec.ts
+++ src/utils/tests/date.spec.ts
@@ -1,18 +1,24 @@
 import { monthsDelta } from '../date';
 
-describe('monthsDelta', () => {
-    it.each`
-        firstDate                                   | secondDate                                    | expected
-        ${new Date(2020, 0, 1)}  | ${new Date(2020, 0, 1)}    | ${0}
-        ${new Date(2020, 3, 1)}  | ${new Date(2020, 0, 1)}    | ${-3}
-        ${new Date(2020, 0, 1)}  | ${new Date(2020, 3, 1)}    | ${3}
-        ${new Date(2020, 0, 15)}  | ${new Date(2020, 3, 17)}  | ${3}
-        ${new Date(2020, 0, 15)}  | ${new Date(2020, 4, 1)}   | ${3}
-        ${new Date(2020, 0, 15)}  | ${new Date(2020, 3, 13)}  | ${2}
-        ${new Date(2020, 0, 30)}  | ${new Date(2020, 1, 1)}  | ${0}
-    `('should round $value with a precision of $precision digits', ({ firstDate, secondDate, expected }) => {
-        const delta = monthsDelta(firstDate, secondDate);
+type TestCase = [Date, Date, number];
 
-        expect(delta).toBe(expected);
+describe('monthsDelta', () => {
+    const testCases: TestCase[] = [
+        [new Date(2020, 0, 1), new Date(2020, 0, 1), 0],
+        [new Date(2020, 3, 1), new Date(2020, 0, 1), -3],
+        [new Date(2020, 0, 1), new Date(2020, 3, 1), 3],
+        [new Date(2020, 0, 15), new Date(2020, 3, 17), 3],
+        [new Date(2020, 0, 15), new Date(2020, 4, 1), 3],
+        [new Date(2020, 0, 15), new Date(2020, 3, 13), 2],
+        [new Date(2020, 0, 30), new Date(2020, 1, 1), 0],
+        [new Date(2020, 2, 31), new Date(2020, 3, 30), 0],
+        [new Date(2020, 2, 30), new Date(2021, 3, 30), 13],
+    ];
+
+    testCases.forEach(([from, to, expected]) => {
+        const testName = `should return ${expected} months between ${from.toDateString()} and ${to.toDateString()}`;
+        const testCase = () => expect(monthsDelta(from, to)).toBe(expected);
+
+        it(testName, testCase);
     });
 });
