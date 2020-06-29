b655706f3d5dcbbf5f9e5b9045716bf6de4620b4
Toby Daigle <toby.daigle@hotmail.ca>
2020-06-29 15:47:15 -0400

    Move stuff around in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import * as ConsumerPriceIndex from './misc/consumer-price-index';
-import * as IQPFStats from './misc/iqpf-stats';
-import * as LifeExpectancy from './misc/life-expectancy';
+import * as LifeIncomeFund from './investments/life-income-fund';
+import * as RegisteredRetirementIncomeFund from './investments/registered-retirement-income-fund';
+import * as RegisteredRetirementSavingsPlan from './investments/registered-retirement-savings-plan';
+import * as TaxFreeSavingsAccount from './investments/tax-free-savings-account';
 
 import * as CanadaPensionPlan from './pension/canada-pension-plan';
 import * as OldAgeSecurity from './pension/old-age-security';
@@ -8,14 +9,13 @@ import * as PublicPensionPlan from './pension/public-pension-plan';
 import * as QuebecPensionPlan from './pension/quebec-pension-plan';
 import * as SupplementalPensionPlan from './pension/supplemental-pension-plan';
 
-import * as LifeIncomeFund from './investments/life-income-fund';
-import * as RegisteredRetirementIncomeFund from './investments/registered-retirement-income-fund';
-import * as RegisteredRetirementSavingsPlan from './investments/registered-retirement-savings-plan';
-import * as TaxFreeSavingsAccount from './investments/tax-free-savings-account';
-
 import * as EmploymentInsurance from './taxes/employment-insurance';
 import * as IncomeTax from './taxes/income-tax';
 import * as QuebecParentalInsurancePlan from './taxes/quebec-parental-insurance-plan';
+
+import * as ConsumerPriceIndex from './misc/consumer-price-index';
+import * as IQPFStats from './misc/iqpf-stats';
+import * as LifeExpectancy from './misc/life-expectancy';
 
 export = {
     INVESTMENTS: {
