a6897e14c67accc40c4fb532e1d85df14710ba2e
Alex Elie <alex.elie@equisoft.com>
2025-02-07 16:12:31 -0500

    two more tests

--- src/investments/registered-education-savings-plan/tests/canada-education-savings-grant.spec.ts
+++ src/investments/registered-education-savings-plan/tests/canada-education-savings-grant.spec.ts
@@ -29,10 +29,36 @@ describe('getTotalForAYear', () => {
         expect(totalGrant).toBe(expectedTotalGrant);
     });
 
+    // eslint-disable-next-line max-len
+    it('should return the correct total grant for a year for low income level with higher than max contribution', () => {
+        const income = IncomeLevel.LOW_INCOME_THRESHOLD;
+        const contribution = CanadaEducationSavingsGrant.MAX_AMOUNT_YEARLY_FOR_GRANT + 1;
+        // eslint-disable-next-line max-len
+        const baseGrant = CanadaEducationSavingsGrant.MAX_AMOUNT_YEARLY_FOR_GRANT * CanadaEducationSavingsGrant.YEARLY_GRANT_PERCENT;
+        // eslint-disable-next-line max-len
+        const suppGrant = CanadaEducationSavingsGrant.MAX_AMOUNT_FOR_SUPP_GRANT * CanadaEducationSavingsGrant.SUPP_GRANT_PERCENT_FOR_LOW_INCOME;
+        const expectedTotalGrant = baseGrant + suppGrant;
+        const totalGrant = CanadaEducationSavingsGrant.getTotalForAYear(income, contribution);
+        expect(totalGrant).toBe(expectedTotalGrant);
+    });
+
     it('should return the correct total grant for a year for medium income level', () => {
         const income = IncomeLevel.MEDIUM_INCOME_THRESHOLD;
         const contribution = CanadaEducationSavingsGrant.MAX_AMOUNT_YEARLY_FOR_GRANT;
         const baseGrant = contribution * CanadaEducationSavingsGrant.YEARLY_GRANT_PERCENT;
+        // eslint-disable-next-line max-len
+        const suppGrant = CanadaEducationSavingsGrant.MAX_AMOUNT_FOR_SUPP_GRANT * CanadaEducationSavingsGrant.SUPP_GRANT_PERCENT_FOR_MEDIUM_INCOME;
+        const expectedTotalGrant = baseGrant + suppGrant;
+        const totalGrant = CanadaEducationSavingsGrant.getTotalForAYear(income, contribution);
+        expect(totalGrant).toBe(expectedTotalGrant);
+    });
+
+    // eslint-disable-next-line max-len
+    it('should return the correct total grant for a year for medium income level with higher than max contribution', () => {
+        const income = IncomeLevel.MEDIUM_INCOME_THRESHOLD;
+        const contribution = CanadaEducationSavingsGrant.MAX_AMOUNT_YEARLY_FOR_GRANT + 1;
+        // eslint-disable-next-line max-len
+        const baseGrant = CanadaEducationSavingsGrant.MAX_AMOUNT_YEARLY_FOR_GRANT * CanadaEducationSavingsGrant.YEARLY_GRANT_PERCENT;
         // eslint-disable-next-line max-len
         const suppGrant = CanadaEducationSavingsGrant.MAX_AMOUNT_FOR_SUPP_GRANT * CanadaEducationSavingsGrant.SUPP_GRANT_PERCENT_FOR_MEDIUM_INCOME;
         const expectedTotalGrant = baseGrant + suppGrant;
