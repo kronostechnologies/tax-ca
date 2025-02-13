580df7bb65dd99256f82d93de8c935aaf865f004
alexelie120 <153525996+alexelie120@users.noreply.github.com>
2025-02-13 12:04:51 -0500

    fix (#543)

--- src/investments/registered-education-savings-plan/index.ts
+++ src/investments/registered-education-savings-plan/index.ts
@@ -1,5 +1,6 @@
 export * from './canada-education-savings-grant';
 export * from './canada-learning-bond';
 export * from './quebec-education-savings-incentive';
+export * from './british-columbia-training-and-education-savings-grant';
 export * from './tuition-fees';
 export * from './income-level';
