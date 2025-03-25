9448809a233fa5c49f8c427a30cd0770da668ca4
Jean-Simon Garneau <js.garneau@gmail.com>
2025-03-25 11:51:17 -0400

    feat(EducationSavingsPlan): added Beneficiary MAX_AGE (#558)

---
+++ src/investments/registered-education-savings-plan/beneficiary.ts
@@ -0,0 +1,18 @@
+/*
+ Sources
+ https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=3710004501&cubeTimeFrame.startYear=2024+%2F+2025&cubeTimeFrame.endYear=2024+%2F+2025&referencePeriods=20240101%2C20240101
+
+ Revised data
+ 2024-09-04
+
+ Last updated
+ 2025-02-07
+ */
+
+export interface Beneficiary {
+    MAX_AGE: number;
+}
+
+export const Beneficiary: Beneficiary = {
+    MAX_AGE: 35,
+};
