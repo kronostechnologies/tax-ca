a8a230eed476ef5c3ed4b4ef0302d8205df7f379
Jean-Simon Garneau <js.garneau@gmail.com>
2025-03-27 11:55:36 -0400

    Removed capitalGains inclusion rate brackets (#559)

--- src/investments/non-registered-savings-plan.ts
+++ src/investments/non-registered-savings-plan.ts
@@ -14,13 +14,8 @@ import { Rate } from '../taxes';
 export const CAPITAL_GAINS_BRACKETS: Rate[] = [
     {
         FROM: 0,
-        TO: 250000,
-        RATE: 0.5,
-    },
-    {
-        FROM: 250000,
         TO: Infinity,
-        RATE: 2 / 3,
+        RATE: 0.5,
     },
 ];
 
--- src/investments/tests/non-registered-retirement-savings-plan.spec.ts
+++ src/investments/tests/non-registered-retirement-savings-plan.spec.ts
@@ -7,11 +7,9 @@ describe('getCapitalGainsTaxableAmount', () => {
             { capitalGains: 0, taxableAmount: 0 },
             { capitalGains: -50, taxableAmount: 0 },
             { capitalGains: 250000, taxableAmount: 125000 },
-            { capitalGains: 350000, taxableAmount: 191666.66666666666 },
-            { capitalGains: 350001, taxableAmount: 191667.3333333333 },
-            { capitalGains: 250100, taxableAmount: 125066.66666666667 },
+            { capitalGains: 350000, taxableAmount: 175000 },
             { capitalGains: 249000, taxableAmount: 124500 },
-            { capitalGains: 250001, taxableAmount: 125000.66666666667 },
+            { capitalGains: 250001, taxableAmount: 125000.5 },
         ];
 
         capitalGainsList.forEach((item) => {
