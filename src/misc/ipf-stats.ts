e1815cf5ff573a5966a50fc3764031eaeec3bdb7
Toby Daigle <toby.daigle@equisoft.com>
2025-05-02 15:24:59 -0400

    ABF-9905 dev(iqpf): Add two new equities and update Canadian Equities value (#572)

--- src/misc/ipf-stats.ts
+++ src/misc/ipf-stats.ts
@@ -9,25 +9,31 @@ export interface ReturnRates {
     SHORT_TERM: number;
     FIXED_INCOME: number;
     CANADIAN_EQUITIES: number;
+    US_EQUITIES: number;
+    INTL_DEVELOPED_MARKET_EQUITIES: number;
+    EMERGING_MARKET_EQUITIES: number;
     CONSERVATIVE_PORTFOLIO: number;
     BALANCED_PORTFOLIO: number;
     DYNAMIC_PORTFOLIO: number;
 }
 
-export interface IQPFStatistics {
+export interface IPFStatistics {
     INFLATION: number;
     PERFORMANCE_RATE: number;
     RETURN_RATES: ReturnRates;
     BORROWING_RATE: number;
 }
 
-export const IQPF: IQPFStatistics = {
+export const IPF: IPFStatistics = {
     INFLATION: 0.021,
     PERFORMANCE_RATE: 0.01,
     RETURN_RATES: {
         SHORT_TERM: 0.024,
         FIXED_INCOME: 0.034,
-        CANADIAN_EQUITIES: 0.064,
+        CANADIAN_EQUITIES: 0.066,
+        US_EQUITIES: 0.066,
+        INTL_DEVELOPED_MARKET_EQUITIES: 0.069,
+        EMERGING_MARKET_EQUITIES: 0.08,
         CONSERVATIVE_PORTFOLIO: 0.028,
         BALANCED_PORTFOLIO: 0.035,
         DYNAMIC_PORTFOLIO: 0.045,
