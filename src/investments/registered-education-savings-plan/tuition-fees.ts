87887abb44b9500a261a7ee26c0c0892cb5bf0b2
Kevin Doucet <kevin.doucet@equisoft.com>
2025-12-21 20:09:25 -0500

    1st batch of changes (RRIF excluded)

--- src/investments/registered-education-savings-plan/tuition-fees.ts
+++ src/investments/registered-education-savings-plan/tuition-fees.ts
@@ -2,11 +2,8 @@
  Sources
  https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=3710004501&cubeTimeFrame.startYear=2024+%2F+2025&cubeTimeFrame.endYear=2024+%2F+2025&referencePeriods=20240101%2C20240101
 
- Revised data
- 2024-09-04
-
  Last updated
- 2025-02-07
+ 2025-12-21
  */
 
 import { ByProvince, ProvinceCode } from '../../misc';
@@ -17,19 +14,19 @@ export interface TuitionFees {
 }
 
 const TuitionFeesData: ByProvince<number> = {
-    AB: 7734,
-    BC: 6607,
-    MB: 5534,
-    NB: 9470,
-    NL: 3727,
-    NS: 9762,
-    PE: 7728,
-    ON: 8514,
-    QC: 3594,
-    SK: 9609,
-    NT: 6000, // valeur hypothétique
-    NU: 6500, // valeur hypothétique
-    YT: 4350,
+    AB: 8067,
+    BC: 6862,
+    MB: 5993,
+    NB: 9938,
+    NL: 3746,
+    NS: 9988,
+    PE: 8191,
+    ON: 8958,
+    QC: 3963,
+    SK: 9863,
+    NT: 7734, // using geography = Canada
+    NU: 7734, // using geography = Canada
+    YT: 4470,
 };
 
 const getTuitionFeesByProvinceCode = (provinceCode: ProvinceCode): number => TuitionFeesData[provinceCode];
